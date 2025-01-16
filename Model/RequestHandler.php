dfaf1c7a3096c4d8952707501c1519626a96d139
Maxime Coudreuse <maxime.coudreuse@benabee.com>
2025-01-16 13:05:16 +0100

    Version 1.2.4

--- Model/RequestHandler.php
+++ Model/RequestHandler.php
@@ -16,8 +16,8 @@ namespace Benabee\ProductManagerConnector\Model;
  */
 class RequestHandler
 {
-    const EXTENSION_VERSION = '1.2.3';
-    const BRIDGE_VERSION = '2.4.2'; // The extension is based on this bridge file version
+    const EXTENSION_VERSION = '1.2.4';
+    const BRIDGE_VERSION = '2.4.4'; // The extension is based on this bridge file version
 
     protected $_magentoConfiguration;
     protected $_database;
